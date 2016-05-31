4271a0539be7add10464cc1bd9d6aa6137899351
Craig Woollett <craigwoollett@outlook.com>
2016-05-31 12:59:54 +1000

    added correct login, can change tr-069 configuration

--- NTC-140/Form1.cs
+++ NTC-140/Form1.cs
@@ -13,19 +13,25 @@ namespace NTC_140
 {
     public partial class Form1 : Form
     {
+        string contentType = "application/x-www-form-urlencoded";
         string modemIP = "http://192.168.1.1/";
         void configureACS()
         { string tr069uri = "TR069.html?server";
             Browser modem = new Browser();
             modem.Navigate(modemIP);
+            //login in
+            string postData = "src_page=&username=admin&password=admin";
+           
+            Uri _uri = new Uri(modemIP);
+            modem.Navigate(_uri, postData, contentType);
+            // you forgot to post username and password you FAT FUCK!!!
             string modemURL = modemIP + tr069uri;
-// you forgot to post username and password you FAT FUCK!!!
             modem.Navigate(modemURL);
 
             //post ACS details
-            string contentType = "application/x-www-form-urlencoded";
-            string postData = "tr069_enable=1&periodic_enable=1&tr069Enable=1&ACSURL=http%3A%2F%2Facs.netcommwireless.com%2Fcpe.php&username=cpe&password=cpe&Vpassword=cpe&ConReq_username=cpe&ConReq_password=cpe&ConReq_Vpassword=cpe&periodicEnable=1&periodic_interval=86399";
-            Uri _uri = new Uri(modemURL);
+          
+            postData = "tr069_enable=1&periodic_enable=1&tr069Enable=1&ACSURL=http%3A%2F%2Facs.netcommwireless.com%2Fcpe.php&username=cpe&password=cpe&Vpassword=cpe&ConReq_username=cpe&ConReq_password=cpe&ConReq_Vpassword=cpe&periodicEnable=1&periodic_interval=86398";
+             _uri = new Uri(modemURL);
             modem.Navigate(_uri, postData, contentType);
 
 
